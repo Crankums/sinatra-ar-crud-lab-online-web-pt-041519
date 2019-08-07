cd7dd7c3a5cd82f76f3836bbe5c962f812d8186b
Craig Donavin <crdonavin@gmail.com>
2019-08-07 14:15:42 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_accessor :title, :content
+  ARTICLE
+
+  def initialize(title, content)
+    @title = title
+    @content = content
+    ARTICLE << self
+  end
+
+  def self.all
+    
+  end
 end
